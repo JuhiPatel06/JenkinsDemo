f593306a554119c211723fc9291af57e95ae8c7e
JuhiPatel06 <144560434+JuhiPatel06@users.noreply.github.com>
2023-10-19 13:51:13 +0100

    Demo for git rebase

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("This is my first gitHub commit integreting with jenkins");
         System.out.println("This is commit to feature into master");
         System.out.println("This demo for no conflict");
+System.out.println("This demo is for rebase from intellij");
 
     }
     void hello(){
