6b8ec8c6dc118807d30bd699054785eb5347dba0
JuhiPatel06 <144560434+JuhiPatel06@users.noreply.github.com>
2023-10-19 13:30:17 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package src.main.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my first gitHub commit integreting with jenkins");
+        System.out.println("This is commit to feature into master");
+        System.out.println("This demo for no conflict");
+
+    }
+    void hello(){
+
+    }
+}
